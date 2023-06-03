d408ad7c2c19e2d84ef33695f3b0cfddec78a9ec
aazimsherazi19 <aazimsherazi3210@gmail.com>
2023-06-03 12:38:34 +0500

    budget app

---
+++ script.js
@@ -0,0 +1,11 @@
+let budget = 0;
+let expenses = [];
+
+function showBudgetPopup() {
+    document.getElementById("popup").style.dispaly = "block";
+}
+
+function hide_popup() {
+    document.getElementById("popup").style.dispaly = "none";
+}
+
